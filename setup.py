5563c62c4699c6506f769be6f77608714eb0f9dd
joamag <joamag@gmail.com>
2014-07-23 20:18:47 +0100

    new version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ import setuptools
 
 setuptools.setup(
     name = "tiberium",
-    version = "0.1.7",
+    version = "0.1.8",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Tiberium System",
